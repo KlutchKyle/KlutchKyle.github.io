24abb77a9398a36affb304e379d922a4c60f228c
Kylethelegend <kylethelegend@Kylethelegends-Mac.local>
2019-03-01 18:29:30 -0800

    Update Winning -> 0.4

--- debs/com.klutchkyle.winning/var/mobile/Library/LockHTML/Winning_0.3/options.js
+++
@@ -1,3 +0,0 @@
-// no options with this version (0.3). Feel free to email me with suggestions :)
-
-
---
+++ debs/com.klutchkyle.winning/var/mobile/Library/LockHTML/Winning_0.4/options.js
@@ -0,0 +1,5 @@
+var test = true; //no options with this version. Feel free to make suggestions! :)
+
+
+
+
