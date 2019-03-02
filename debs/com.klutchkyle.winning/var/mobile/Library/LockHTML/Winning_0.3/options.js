423186faa4b7099a0379441765a7001227e19d76
Kylethelegend <kylethelegend@Kylethelegends-Mac.local>
2019-03-01 17:57:00 -0800

    Update Winning -> 0.3

--- debs/com.klutchkyle.winning/var/mobile/Library/LockHTML/Winning_0.3/options.js
+++ debs/com.klutchkyle.winning/var/mobile/Library/LockHTML/Winning_0.3/options.js
@@ -1,7 +1,5 @@
 var darkmode = false; // Toggle dark mode
-var clock = true; // Show the clock
 var date = true; // Show date
-var twelvehour = false; // Toggle 12/24-hour clock
+var twelvehour = false;
 var battery = false; // Toggle battery view (requires InfoStats)
 var wifi = true;
-var fadein = true; //Fade in widget after locking your device 
