0fa584959086ea3c9357f6e3f04111de04ffa05b
Kylethelegend <kylethelegend@Kylethelegends-Mac.local>
2019-05-25 11:47:07 -0700

    helloit _ update Pan3ls _ Update

--- debs/com.bestware.helloit/var/mobile/Library/LockHTML/Helloit/options.js
+++ debs/com.bestware.helloit/var/mobile/Library/LockHTML/Helloit/options.js
@@ -1,3 +1,6 @@
 var matchColor = true; // matchcolor with LS Wallpaper
-var domatch = "Muted"; //Choose from Vibrant, LightVibrant, DarkVibrant, Muted, LightMuted, DarkMuted
+var domatch = "LightVibrant"; //Choose from Vibrant, LightVibrant, DarkVibrant, Muted, LightMuted, DarkMuted
 var Clock = "12h";  //12h or 24h format clock
+var colour = "#00ff00"; //custom color for text
+var colored = "H"; //colorized part of custom text
+var uncolored = "ELLO"; //uncolorized part of custom text
--- packages.sh
+++ packages.sh
@@ -1,1 +1,2 @@
 dpkg-deb -b debs/com.bestware.helloit
+dpkg-deb -b debs/com.klutchkyle.pan3ls
