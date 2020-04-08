ae1e293aaf5c366faed7bb68d11e92224b831138
KlutchKyle <47758911+KlutchKyle@users.noreply.github.com>
2020-04-08 14:53:39 -0700

    Fix steamyshower colors

--- debs/com.klutchkyle.steamyshower/var/mobile/Library/LockHTML/SteamyShower/options.js
+++ debs/com.klutchkyle.steamyshower/var/mobile/Library/LockHTML/SteamyShower/options.js
@@ -1,6 +1,6 @@
 var blurRadius = 10; // Set blur Intensity (COMING SOON!)
 var blurRed = 255;  // Set blur Red Level (RGB Values)
-var blurGreen = 255; // Set blur Green Level (RGB Values)
-var blurBlue = 255; // Set blur Blue Level (RGB Values)
+var blurGreen = 0; // Set blur Green Level (RGB Values)
+var blurBlue = 0; // Set blur Blue Level (RGB Values)
 var blurOpacity = 0.1; // Set blur opacity
 var matchColor = true; // Wallpaper blur color matching toggle
