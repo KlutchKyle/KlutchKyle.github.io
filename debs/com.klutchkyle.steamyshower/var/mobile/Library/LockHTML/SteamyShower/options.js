5f636c73fec262a0f7b11c6bf39f263d1f105d02
KlutchKyle <47758911+KlutchKyle@users.noreply.github.com>
2020-04-08 15:18:23 -0700

    add light vs dark color match option steamyshower

--- debs/com.klutchkyle.steamyshower/var/mobile/Library/LockHTML/SteamyShower/options.js
+++ debs/com.klutchkyle.steamyshower/var/mobile/Library/LockHTML/SteamyShower/options.js
@@ -1,6 +1,7 @@
-var blurRadius = 10; // Set blur Intensity (COMING SOON!)
-var blurRed = 255;  // Set blur Red Level (RGB Values)
-var blurGreen = 0; // Set blur Green Level (RGB Values)
-var blurBlue = 0; // Set blur Blue Level (RGB Values)
+var blurRadius = 10; // Set blur Intensity  (COMING SOON!)
+var blurRed = 255;  // Set blur Red Level when matchColor is false (RGB Values)
+var blurGreen = 0; // Set blur Green Level when matchColor is false (RGB Values)
+var blurBlue = 0; // Set blur Blue Level when matchColor is false (RGB Values)
 var blurOpacity = 0.1; // Set blur opacity
 var matchColor = true; // Wallpaper blur color matching toggle
+var lightColor = true; // Match light color? Dark vibrant when false
